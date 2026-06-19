3d54ec8fdce661696200bc3539ad9e721f467b36
yanagi <yanagiibuki3400@gmail.com>
2026-06-19 17:26:01 +0900

    アステロイド修正

--- BaseProject 3/BaseProject/Src/Object/Weapon/Asteroid/Asteroid.cpp
+++ BaseProject 3/BaseProject/Src/Object/Weapon/Asteroid/Asteroid.cpp
@@ -27,6 +27,7 @@ void Asteroid::WeaponLoad(void)
 void Asteroid::WeaponInit(void)
 {
 	trans.scale = Vector3(GetParameter("Init", "scale"));
+	trans.pos += GetParameter("Init", "modelOffset");
 }
 
 void Asteroid::WeaponUpdate(void)
@@ -37,6 +38,48 @@ void Asteroid::WeaponUpdate(void)
 
 void Asteroid::WeaponDraw(void)
 {
+	auto frameIndex = MV1SearchFrame(ownerTrans_->get().model, "mixamorig:RightHandIndex2");
+	auto framePos = MV1GetFramePosition(ownerTrans_->get().model, frameIndex);
+	auto handMatrix = MV1GetFrameLocalWorldMatrix(ownerTrans_->get().model, frameIndex);
+	auto vec = VScale({ 0.0f,0.1f,0.0f }, 1.0 / 0.025);
+	auto swordStartPos = VTransform(vec, handMatrix);
+
+	VECTOR xAxis =
+	{
+		handMatrix.m[0][0],
+		handMatrix.m[0][1],
+		handMatrix.m[0][2]
+	};
+
+	VECTOR yAxis =
+	{
+		handMatrix.m[1][0],
+		handMatrix.m[1][1],
+		handMatrix.m[1][2]
+	};
+
+	VECTOR zAxis =
+	{
+		handMatrix.m[2][0],
+		handMatrix.m[2][1],
+		handMatrix.m[2][2]
+	};
+
+	DrawLine3D(
+		swordStartPos,
+		VAdd(swordStartPos, VScale(xAxis, 20.0f)),
+		GetColor(255, 0, 0));
+
+	DrawLine3D(
+		swordStartPos,
+		VAdd(swordStartPos, VScale(yAxis, 20.0f)),
+		GetColor(0, 255, 0));
+
+	DrawLine3D(
+		swordStartPos,
+		VAdd(swordStartPos, VScale(zAxis, 20.0f)),
+		GetColor(0, 0, 255));
+
 }
 
 void Asteroid::WeaponAlphaDraw(void)
